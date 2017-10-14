68fd214a379e8fee71fb734d0d4e4215f1fdee4e
Sid Bhargava <sid@cheq.ca>
2017-10-14 10:22:47 -0400

    added api url and parameter for python service

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 #!flask/bin/python
 from flask import Flask
+from flask import abort
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "LETS DO THIS"
+
+@app.route('/jobs/api/v1.0/likelihoods/<string:job_title>', methods=['GET'])
+def get_likelihood(job_title):
+    if len(job_title) == 0:
+        abort(404)
+    return jsonify({'likelihood': '12%'})
 
 if __name__ == '__main__':
     app.run(debug=True)
